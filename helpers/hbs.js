a982f0dde84f29e5c16838cc61d06a6de779751c
faisalsayed10 <faisal.sayed502@gmail.com>
2020-12-21 13:14:03 +0530

    view user

--- helpers/hbs.js
+++ helpers/hbs.js
@@ -15,7 +15,9 @@ module.exports = {
     return str;
   },
   stripTags: function (input) {
-    return input.replace(/<(?:.|\n)*?>/gm, "");
+    let removedTags = input.replace(/<(?:.|\n)*?>/gm, "");
+    let removednbsp = removedTags.replace(/(&nbsp;)/gm, " ");
+    return removednbsp;
   },
   editIcon: function (storyUser, loggedUser, storyId, floating = true) {
     if (storyUser._id.toString() == loggedUser._id.toString()) {
--- routes/stories.js
+++ routes/stories.js
@@ -35,7 +35,7 @@ router.get("/", ensureAuth, async (req, res) => {
 
 router.get("/:id", ensureAuth, async (req, res) => {
   try {
-    let story = (await Story.findById(req.params.id)).populate('user').lean()
+    let story = await Story.findById(req.params.id).populate("user").lean();
 
     if (!story) {
       return res.render('error/404')
@@ -103,5 +103,16 @@ router.delete("/:id", ensureAuth, async (req, res) => {
     return res.render('error/500')
   }
 });
+
+router.get('/user/:userId', ensureAuth, async (req, res) => {
+  try {
+    const stories = await Story.find({ user: req.params.userId, status: 'public' }).populate('user').lean();
+
+    res.render('stories/index', { stories })
+  } catch (err) {
+      console.error(err)
+      res.render('error/404')
+  }
+})
 
 module.exports = router;
